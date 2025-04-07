b8a6bd49ce25fa314e5727baf152b2d9ce370cc5
kalyanroy26 <kalyanroy8143@gmail.com>
2025-04-07 10:00:46 +0530

    task completed

--- Day-25/task.py
+++ Day-25/task.py
@@ -1,11 +1,13 @@
 list = [[1,2,3,16],[10,15,12],[4,5,6],[7,8,9]]
 
-# for i in range(len(list)):
-#     for j in range(len(list[i])):
-#         print(list[i][j],end=" ")
-
-#max elements in sub list
-sub_list=[]
+#max element in sub list
+max_sub_list=[]
 for i in list:
-    sub_list.append(max(i))
-print(sub_list)
+    max_sub_list.append(max(i))
+print("max: ",max(max_sub_list))
+
+#min element in sub list
+min_sub_list=[]
+for i in list:
+    min_sub_list.append(min(i))
+print("minimum: ",min(min_sub_list))
