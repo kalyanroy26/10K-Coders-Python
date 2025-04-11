d05db3a3262d1a346098c85e10e246adc9f5bf24
kalyanroy26 <kalyanroy8143@gmail.com>
2025-04-11 12:58:21 +0530

    day 28 added

---
+++ Day-28/task.py
@@ -0,0 +1,11 @@
+#TASK 1 FIND MISSING NUMBER IN A LIST
+num = [2,3,4,6,8,10]
+
+for i in range(1,max(num)):
+    if i not in num:
+        print(f"{i} is missing number.")
+
+#TASK 2 
+details = {
+    
+}
