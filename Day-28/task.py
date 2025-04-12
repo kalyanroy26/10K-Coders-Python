683e724acb9141f8596664397e2d3868b05d74e8
kalyanroy26 <kalyanroy8143@gmail.com>
2025-04-12 09:30:18 +0530

    day 28 task added

--- Day-28/task.py
+++ Day-28/task.py
@@ -5,7 +5,17 @@ for i in range(1,max(num)):
     if i not in num:
         print(f"{i} is missing number.")
 
-#TASK 2 
-details = {
-    
+#TASK 2 Take a dictionary with salaries and Find percentage of the salaries
+data = {
+    'kalyan': 700000,
+    'naveen': 1000000
 }
+
+total = 0
+
+for key in data:
+    total+=data[key]
+
+for key in data:
+    perc = data[key]/total *100
+    print(f"{key} salary percentage: {perc.__ceil__()}%")
