de5853fb5be1d7348f58b5f159093c6f243b678e
Kalyan Maharajulu <94466266+kalyanroy26@users.noreply.github.com>
2025-04-17 11:16:47 +0530

    Update task.py

--- Day-31/task.py
+++ Day-31/task.py
@@ -1,24 +1,25 @@
-# str = "teja uday"
-# str = str.split(" ")
-# new = ""
+# PROGRAM TO CONVERT FIRST LETTER OF A WORD TO UPPERCASE
+str = "teja uday"
+str = str.split(" ")
+new = ""
 
-# for i in str:
-#     new+=i.title()+" "
+for i in str:
+    new+=i.title()+" "
 
-# print(new)
+print(new)
 
+#PROGRAM TO PRINT CHARACTER WITH HIGHEST OCCURENCES
+sentence = "dont trouble the trouble"
+char_count = {}
+for i in sentence:
+    char_count[i] = char_count.get(i,0)+1
 
-# sentence = "dont trouble the trouble"
-# char_count = {}
-# for i in sentence:
-#     char_count[i] = char_count.get(i,0)+1
+c=0
+max = ''
 
-# c=0
-# max = ''
+for key in char_count:
+    if char_count[key]>c:
+        c=char_count[key]
+        max = key
 
-# for key in char_count:
-#     if char_count[key]>c:
-#         c=char_count[key]
-#         max = key
-
-# print(max,c)
+print(max,c)
