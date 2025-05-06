5870fd1e233549a39ba876096a1caf3629ef418c
kalyanroy26 <kalyanroy8143@gmail.com>
2025-05-06 12:11:44 +0530

    day 40 pattern task added

--- Day-40/task.py
+++ Day-40/task.py
@@ -1,18 +1,15 @@
 
 rows =5
 code = 97
-for row in range(1,rows+1):
-    if row == 1:
-        print(chr(code))
+
+for r in range(1,rows+1):
+    res=""
+    for c in range(1,r+1):
+        if r == rows or c ==1 or c ==  r:
+            res+=chr(code)+" "
+        else:
+            res+=" "+" "
+
         code+=1
-    elif row == 2:
-        print(chr(code),chr(code+1))
-        code+=2
-    elif row == 3:
-        print(chr(code)," ",chr(code+2))
-        code+=3
-    elif row == 4:
-        print(chr(code)," "," ",chr(code+3))
-        code+=4
-    elif row == 5:
-        print(chr(code),chr(code+1),chr(code+2),chr(code+3))
+    print(res)
+    
