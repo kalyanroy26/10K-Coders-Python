e5b4f6266258e0bad5415f5e61cf42d5641341aa
kalyanroy26 <kalyanroy8143@gmail.com>
2025-05-09 10:38:35 +0530

    Hourglass and diamond Patterns added

---
+++ Day-44/task.py
@@ -0,0 +1,70 @@
+#Diamond Pattern
+rows = 5
+for row in range(1,2*rows):
+    res = ""
+    space = rows-row if row<rows else row-rows
+    cols = row if row<=rows else 2*rows-row
+
+    for sp in range(1,space+1):
+        res+=" "
+    for col in range(1,cols+1):
+        res+=str(col)+" "
+    print(res)
+
+#     1 
+#    2 2 
+#   3 3 3
+#  4 4 4 4
+# 5 5 5 5 5
+#  6 6 6 6
+#   7 7 7
+#    8 8
+#     9
+
+
+#     * 
+#    * * 
+#   * * *
+#  * * * *
+# * * * * *
+#  * * * *
+#   * * *
+#    * *
+#     *
+
+
+#Hourglass Pattern
+
+rows = 5
+for row in range(1,2*rows):
+    res = ""
+    space = row-1 if row<rows else 2*rows-row-1 
+    cols = rows-row+1 if row<=rows else row-rows+1
+
+    for sp in range(1,space+1):
+        res+=" "
+    for col in range(1,cols+1):
+        res+=str(col)+" "
+    print(res)
+
+
+# 1 2 3 4 5 
+#  1 2 3 4 
+#   1 2 3
+#    1 2
+#     1
+#    1 2
+#   1 2 3
+#  1 2 3 4
+# 1 2 3 4 5
+
+
+#  * * * * 
+#   * * * 
+#    * * 
+#     * 
+#    * * 
+#   * * * 
+#  * * * * 
+# * * * * * 
+    
