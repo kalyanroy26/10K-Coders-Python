536cef7b2aea98c9bc118b8f013f808da5cc4df6
kalyanroy26 <kalyanroy8143@gmail.com>
2025-06-27 22:21:27 +0530

    pattern task added

--- Advanced Python/Day-1/task.py
+++ Advanced Python/Day-1/task.py
@@ -1,11 +1,13 @@
-file = open('sample.txt','r')
-# data = ['you do not rise \n','to the level of your goals\n', 'you will fall to\n','the level of your systems\n']
-# file.writelines(data)
+# file = open('sample.txt','r')
+# # data = ['you do not rise \n','to the level of your goals\n', 'you will fall to\n','the level of your systems\n']
+# # file.writelines(data)
 
-data = file.readlines()
-word = ''
+# data = file.readlines()
+# word = ''
 
-for ele in data:
-    word+=ele[0]
+# for ele in data:
+#     word+=ele[0]
 
-print(word)
+# print(word)
+
+
---
+++ Advanced Python/Day-2/task.py
@@ -0,0 +1,53 @@
+# rows = 5
+
+# for row in range(1,rows+1):
+#     res = ""
+#     for col in range(1,rows+1):
+#         res+=str(col)+" "
+#     print(res)
+
+
+# rows = 5
+
+# for row in range(1,rows+1):
+#     res=""
+#     for col in range(1,rows+1):
+#         if col==1 or col==rows or col==row:
+#             res+="*"+" "
+#         else:
+#             res+="  "
+#     print(res)
+
+
+# rows = 5
+
+# for row in range(1,rows+1):
+#     res=""
+#     for col in range(1,rows+1):
+#         if row==1 or row==rows or col+row==rows+1:
+#             res+="*"+" "
+#         else:
+#             res+=" "+" "
+#     print(res)
+
+
+# rows = 5
+
+# for row in range(1,rows+1):
+#     res=""
+#     for col in range(1,rows+1):
+#         if col==1 or col==rows or row==rows//2+1:
+#             res+="*"+" "
+#         else:
+#             res+=" "+" "
+#     print(res)
+
+# rows=5
+# for row in range(1,rows+1):
+#     res=""
+#     for col in range(1,rows+1):
+#         if row==1 or row==rows or col==rows//2+1:
+#             res+="*"+" "
+#         else:
+#             res+=" "+" "
+#     print(res)
